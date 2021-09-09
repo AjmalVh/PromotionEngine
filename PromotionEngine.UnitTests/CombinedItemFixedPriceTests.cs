a5a4824f9fbaba0643e10fd0ee44273dd94e6f78
Ajmal Hassainar <ajmalcpp@gmail.com>
2021-09-09 14:37:57 +0100

    Added class for CombinedItemFixedPrice

---
+++ PromotionEngine.UnitTests/CombinedItemFixedPriceTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.UnitTests
+{
+    public class CombinedItemFixedPriceTests
+    {
+        [Test]
+        public void PromotionApplicable_If_Already_Applied_ForThis_SKU_ShouldBe_False()
+        {
+          
+        }
+    }
+}
---
+++ PromotionEngine/PromotionRules/CombinedItemFixedPrice.cs
@@ -0,0 +1,34 @@
+﻿using PromotionEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.PromotionRules
+{
+    public class CombinedItemFixedPrice : IPromotionRule
+    {
+        public decimal FixedPrice { get; set; }
+
+        /// <summary>
+        /// SKU of product 1 combo offer
+        /// </summary>
+        public string SKU1 { get; set; }
+
+        /// <summary>
+        /// SKU of product 2 combo offer
+        /// </summary>
+        public string SKU2 { get; set; }
+
+        public decimal CalculateDiscount(Cart cart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsApplicable(Cart cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
