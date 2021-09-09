08a8cbd8881f9d0b569def19abb3d517f8e36291
Ajmal Hassainar <ajmalcpp@gmail.com>
2021-09-09 20:47:50 +0100

    Added test case for Mutually Exclusive promo per SKU

--- PromotionEngine.UnitTests/CartTests.cs
+++ PromotionEngine.UnitTests/CartTests.cs
@@ -105,6 +105,40 @@ namespace PromotionEngine.UnitTests
             totalPrice.Should().Be(280);
         }
 
+        [Test]
+        public void Calculate_Total_Price_Promotion_Mutually_Exclusive_ForSKU()
+        {
+            // The promotion rules are mutually exclusive, that implies only one promotion (individual SKU or combined) is applicable to a particular SKU
+
+            // 4 * A   130 + 50
+            // 1 * B   30
+            // Total = 210
+
+            // 3 of A's for 130
+            IPromotionRule nItemsForFixedPriceA = new NItemsForFixedPrice("A", 3, 130);
+
+            // A & B for 70
+            // (This promotion should not get applied, because the previous promotion for A already got applied for A.)
+            List<string> comboPromoSkus = new() { "A", "B" };
+            IPromotionRule combinedItemFixedPrice = new CombinedItemFixedPrice(comboPromoSkus, 70);
+
+            List<IPromotionRule> activePromotions = new()
+            {
+                nItemsForFixedPriceA,
+                combinedItemFixedPrice
+            };
+
+            Product productA = new("A", 50);
+            Product productB = new("B", 30);
+
+            cart.AddToCart(productA, 4);
+            cart.AddToCart(productB, 1);
+
+            var totalPrice = cart.CalculateTotalPrice(activePromotions);
+
+            totalPrice.Should().Be(210);
+        }
+
         private IEnumerable<IPromotionRule> GetAllActivePromotions()
         {
             /*
